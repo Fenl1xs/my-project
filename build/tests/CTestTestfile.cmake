7bf1cc44ab0196f0b233b5f5114ddf7997e03c43
Danil Bondarenko <danilbondarenko@MacBook-Pro-Danil.local>
2025-09-15 14:28:26 +0300

    Initial commit

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/danilbondarenko/rpn_calculator/tests
+# Build directory: /Users/danilbondarenko/rpn_calculator/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[rpn_tests]=] "/Users/danilbondarenko/rpn_calculator/build/tests/rpn_tests")
+set_tests_properties([=[rpn_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/danilbondarenko/rpn_calculator/tests/CMakeLists.txt;19;add_test;/Users/danilbondarenko/rpn_calculator/tests/CMakeLists.txt;0;")
